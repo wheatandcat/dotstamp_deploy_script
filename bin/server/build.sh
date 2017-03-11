5ccb73955a21a54327d8d9afa5dd03e136a043e3
“mugenxnoheya” <mugenxnoheya@gmail.com>
2017-03-11 13:56:29 +0900

    tmp

--- bin/server/build.sh
+++ bin/server/build.sh
@@ -1,4 +1,2 @@
-rootDir=/vagrant/project/
-
 go build /home/vagrant/go/src/dotstamp_server/main.go
-mv /vagrant/project/dotstamp_server/main /vagrant/project/dotstamp_deploy/dotstamp_server
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/dotstamp_server
