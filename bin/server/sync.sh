35accd5f5c51cd22173b74b9da8fe378993a2cc1
“mugenxnoheya” <mugenxnoheya@gmail.com>
2017-03-12 04:25:07 +0900

    初期

--- bin/server/sync.sh
+++ bin/server/sync.sh
@@ -4,3 +4,5 @@ distDir=/vagrant/project/dotstamp_deploy
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/conf/  $distDir/conf
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/views/ $distDir/views
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/static/  $distDir/static
+rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/db/migrations/  $distDir/db/migrations
+rsync -arv --copy-links --exclude="*.go" --exclude=".*" --exclude="metas" --delete $rootDir/tasks/  $distDir/tasks
