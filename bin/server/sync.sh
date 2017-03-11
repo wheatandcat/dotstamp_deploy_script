b9ae9715de24e85517f869c1f1f8dac3504b60eb
“mugenxnoheya” <mugenxnoheya@gmail.com>
2017-03-11 13:05:44 +0900

    初期

--- bin/server/sync.sh
+++ bin/server/sync.sh
@@ -1,8 +1,6 @@
 rootDir=/vagrant/project/dotstamp_server
+distDir=/vagrant/project/dotstamp_deploy
 
-rsync -arv -e "ssh -p 10020" --copy-links --exclude=".*" --exclude="*.*" $rootDir/static/ worker@160.16.118.176:/project/dotstamp_server/blue/static
-rsync -arv -e "ssh -p 10020" --copy-links --exclude=".*" --exclude="*.*" $rootDir/static/ worker@160.16.118.176:/project/dotstamp_server/green/static
-rsync -arv -e "ssh -p 10020" --copy-links --exclude="*.go" $rootDir/dotstamp_server worker@160.16.118.176:/project/dotstamp_server
-rsync -arv -e "ssh -p 10020" --copy-links --exclude="*.go" $rootDir/tasks/ worker@160.16.118.176:/project/dotstamp_server/tasks
-rsync -arv -e "ssh -p 10020" --copy-links --exclude="*.go" $rootDir/conf/ worker@160.16.118.176:/project/dotstamp_server/conf
-rsync -arv -e "ssh -p 10020" --copy-links --exclude="*.go" $rootDir/views/ worker@160.16.118.176:/project/dotstamp_server/views
+rsync -arv --copy-links --exclude="*.go" $rootDir/conf/  $distDir/conf
+rsync -arv --copy-links --exclude="*.go" $rootDir/views/ $distDir/views
+rsync -arv --copy-links --exclude="*.go" $rootDir/static/  $distDir/static
