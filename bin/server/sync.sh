2435afe017478dfd77f48a61a87c94cc4e968003
“wheatandcat” <wheatandcat@gmail.com>
2017-03-16 15:05:40 +0900

    tasks追加

--- bin/server/sync.sh
+++ bin/server/sync.sh
@@ -5,6 +5,5 @@ rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/conf/ 
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/views/ $distDir/views
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/static/  $distDir/static
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/resources/  $distDir/resources
-rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/tasks/  $distDir/tasks
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/db/migrations/  $distDir/db/migrations
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --exclude="metas" --delete $rootDir/tasks/  $distDir/tasks
