3d716f721f1e62d5dc51a14ada563a22c6e45086
“wheatandcat” <wheatandcat@gmail.com>
2017-03-16 10:34:45 +0900

    csvフォルダ追加

--- bin/server/sync.sh
+++ bin/server/sync.sh
@@ -4,5 +4,6 @@ distDir=/vagrant/project/dotstamp_deploy
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/conf/  $distDir/conf
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/views/ $distDir/views
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/static/  $distDir/static
+rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/resources/  $distDir/resources
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/db/migrations/  $distDir/db/migrations
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --exclude="metas" --delete $rootDir/tasks/  $distDir/tasks
