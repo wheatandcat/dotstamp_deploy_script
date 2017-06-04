69a08f13f858f4a44ff3a14d3dd5226dcdb3823f
“wheatandcat” <wheatandcat@gmail.com>
2017-06-04 21:25:16 +0900

    build tasks

--- bin/server/build.sh
+++ bin/server/build.sh
@@ -1,2 +1,14 @@
 go build /home/vagrant/go/src/dotstamp_server/main.go
 mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/dotstamp_server
+
+go build /home/vagrant/go/src/dotstamp_server/tasks/contributionSearch/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/contributionSearch/contributionSearch
+
+go build /home/vagrant/go/src/dotstamp_server/tasks/contributionTotalFollows/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/contributionTotalFollows/contributionTotalFollows
+
+go build /home/vagrant/go/src/dotstamp_server/tasks/makeMovie/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/makeMovie/makeMovie
+
+go build /home/vagrant/go/src/dotstamp_server/tasks/removeContribution/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/removeContribution/removeContribution
