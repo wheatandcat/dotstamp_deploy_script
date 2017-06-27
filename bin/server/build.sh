8a505be67f75be18eecf71f36ccbc92cce2041e2
“wheatandcat” <wheatandcat@gmail.com>
2017-06-28 03:29:22 +0900

    add tasks

--- bin/server/build.sh
+++ bin/server/build.sh
@@ -1,20 +1,2 @@
-go build /home/vagrant/go/src/dotstamp_server/main.go
+go build /home/vagrant/go/src/github.com/wheatandcat/dotstamp_server/main.go
 mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/dotstamp_server
-
-go build /home/vagrant/go/src/dotstamp_server/tasks/contributionSearch/main.go
-mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/contributionSearch/contributionSearch
-
-go build /home/vagrant/go/src/dotstamp_server/tasks/contributionTotalFollows/main.go
-mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/contributionTotalFollows/contributionTotalFollows
-
-go build /home/vagrant/go/src/dotstamp_server/tasks/makeMovie/main.go
-mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/makeMovie/makeMovie
-
-go build /home/vagrant/go/src/dotstamp_server/tasks/sitemap/main.go
-mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/sitemap/sitemap
-
-go build /home/vagrant/go/src/dotstamp_server/tasks/removeContribution/main.go
-mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/removeContribution/removeContribution
-
-go build /home/vagrant/go/src/dotstamp_server/tasks/soundTime/main.go
-mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/soundTime/soundTime
--- bin/server/sync.sh
+++ bin/server/sync.sh
@@ -7,3 +7,21 @@ rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/static
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/resources/  $distDir/resources
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --delete $rootDir/db/migrations/  $distDir/db/migrations
 rsync -arv --copy-links --exclude="*.go" --exclude=".*" --exclude="metas" --delete $rootDir/tasks/  $distDir/tasks
+
+go build /home/vagrant/go/src/github.com/wheatandcat/dotstamp_server/tasks/contributionSearch/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/contributionSearch/contributionSearch
+
+go build /home/vagrant/go/src/github.com/wheatandcat/dotstamp_server/tasks/contributionTotalFollows/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/contributionTotalFollows/contributionTotalFollows
+
+go build /home/vagrant/go/src/github.com/wheatandcat/dotstamp_server/tasks/makeMovie/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/makeMovie/makeMovie
+
+go build /home/vagrant/go/src/github.com/wheatandcat/dotstamp_server/tasks/sitemap/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/sitemap/sitemap
+
+go build /home/vagrant/go/src/github.com/wheatandcat/dotstamp_server/tasks/removeContribution/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/removeContribution/removeContribution
+
+go build /home/vagrant/go/src/github.com/wheatandcat/dotstamp_server/tasks/soundTime/main.go
+mv /vagrant/project/dotstamp_deploy_script/main /vagrant/project/dotstamp_deploy/tasks/soundTime/soundTime
